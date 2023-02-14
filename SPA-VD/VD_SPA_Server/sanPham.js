3527901491e5b9ad265efbbf77b4dad9064b7c1c
TOAN\PC <toannhph23989@fpt.edu.vn>
2023-02-14 13:39:16 +0700

    update

--- SPA-VD/VD_SPA_Server/sanPham.js
+++ SPA-VD/VD_SPA_Server/sanPham.js
@@ -21,4 +21,32 @@ window.spController = function($scope,$http,$location){
         });
 
     };
+
+    $scope.loadSanPhamForm = function(id){
+        $http.get('http://localhost:3000/sanPham/'+id).then(function(response){
+        console.log(response);
+        if (response.statusText === "OK") {
+            $scope.id = response.data.id;
+            $scope.name = response.data.name;
+            $scope.price = response.data.price;
+            $scope.quantity = response.data.quantity;
+            $scope.discount = response.data.discount;
+        }
+    });
+    };
+
+$scope.updateSanPham = function(){
+    $http.put('http://localhost:3000/sanPham/'+$scope.id,{
+        id: $scope.id,
+        name: $scope.tenSP,
+        price: $scope.price,
+        quantity: $scope.quantity,
+        discount: $scope.discount,
+    }).then(function(response){
+        if (response.status ===200){
+            alert("Sua sản phẩm thành công");
+        }
+    });
+};
+
 }
